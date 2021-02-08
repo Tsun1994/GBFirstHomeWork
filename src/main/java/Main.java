9ccff2842d526335399feeb360375cc19af3b6e9
Alexandr <tank006@yandex.ru>
2021-02-08 16:21:21 +0300

    commit text

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        byte b = 124;
-    }
-}
---
+++ src/main/java/homeworkone/Main.java
@@ -0,0 +1,21 @@
+package homeworkone;
+
+public class Main {
+    public static void main (String[] args) {
+        byte b1 = 1;
+        short sh = 10;
+        int in = 100;
+        long lL = 45L;
+        float fk = 5.5f;
+        double dd = 1.54;
+        char c = '^';
+        String s2f = "Привет, Андрей!";
+        boolean is = true;
+//        System.out.println(HW1(2, 4, 6,6));
+//        System.out.println(HW2 (5, 16));
+//        System.out.println(HW3(-9));
+//        System.out.println(HW4(0));
+//        System.out.println(HW5 (4));
+//        System.out.println (4 % 400);
+    }
+}
