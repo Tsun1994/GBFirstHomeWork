91023b9fcbf6260b1031d5c15bbf439aa868c21a
Alexandr <tank006@yandex.ru>
2021-02-08 16:27:20 +0300

    commit text

--- src/main/java/homeworkone/Main.java
+++ src/main/java/homeworkone/Main.java
@@ -11,11 +11,21 @@ public class Main {
         char c = '^';
         String s2f = "Привет, Андрей!";
         boolean is = true;
-//        System.out.println(HW1(2, 4, 6,6));
+        System.out.println(HW1(2, 4, 6,6));
 //        System.out.println(HW2 (5, 16));
 //        System.out.println(HW3(-9));
 //        System.out.println(HW4(0));
 //        System.out.println(HW5 (4));
 //        System.out.println (4 % 400);
+    }
+    public static float HW1 (float a, float b, float c, float d) {
+        return (a * (b + (c / d)));
+    }
+    public static boolean HW2 (int a1, int a2) {
+        if ((a1 + a2) >= 10 && (a1 + a2) <= 20) {
+            return (true);
+        } else {
+            return (false);
+        }
     }
 }
