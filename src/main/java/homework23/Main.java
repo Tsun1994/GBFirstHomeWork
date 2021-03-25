ba3b37802a6a209807c0612a62ac33044659c58e
Alexandr <tank006@yandex.ru>
2021-03-25 06:57:25 +0400

    Finish

---
+++ src/main/java/homework23/Main.java
@@ -0,0 +1,12 @@
+package homework23;
+
+import java.util.List;
+
+public class Main {
+    public static void main (String [] args){
+        PhoneInfo info = new PhoneInfo();
+//        info.getNameInfo();
+        //info.printInfo("Falop", "4654", 0);
+//        PhoneInfo.addInfo(info.getNameInfo(), info.getPhoneInfo(), 6);
+    }
+}
